72846d3266163effa893abe01ce73d4d66884ff7
Tewodrosyeteji <teddytejinesh@gmail.com>
2022-07-06 23:44:31 +0300

    send mail

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\testEmail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,5 +24,13 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/change-password', [App\Http\Controllers\HomeController::class, 'changePassword'])->name('change-password');
 Route::post('/change-password', [App\Http\Controllers\HomeController::class, 'updatePassword'])->name('update-password');
+Route::get('send-mail',function(){
+    $maildata=[
+        "name"=>"test name",
+        "dob"=>"12/34/3412"
+    ];
+    Mail::to("hello@example.com")->send(new testEmail($maildata));
+    dd('mail send successfully');
+});
 
 
